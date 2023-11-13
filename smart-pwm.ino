8891c5edebbb04ddc92a3811958a00175180f243
Michael <michael.ahli99@gmail.com>
2023-11-13 16:10:47 +0700

    feat: 🎸 add menu functionality
    selectable menu with configurable options

--- smart-pwm.ino
+++ smart-pwm.ino
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -22,6 +24,77 @@ unsigned long dimInterval = 100; // Interval for dimming in milliseconds
 
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+class Button
+{                                             // a simple class for buttons based on the "Debounce" example
+    const byte buttonPin;                     // the GPIO / pin for the button
+    static constexpr byte debounceDelay = 30; // the debounce time; increase if the output flickers. Static because we only need one value for all buttons
+    const bool active;                        // is the pin active HIGH or active LOW (will also activate the pullups!)
+    bool lastButtonState = HIGH;              // the previous reading from the input pin
+    byte lastDebounceTime = 0;                // the last time the output pin was toggled - we check only ONE byte, so I didn't mess around with unsigned long
+
+public:
+    /**
+       \brief constructor for a button
+
+       The constructor takes the GPIO as parameter.
+       If you omit the second parameter, the library will activate the internal pullup resistor
+       and the button should connect to GND.
+       If you set the second parameter to HIGH, the button is active HIGH.
+       The button should connect to VCC.
+       The internal pullups will not be used but you will need an external pulldown resistor.
+
+       \param attachTo the GPIO for the button
+       \param active LOW (default) - if button connects to GND, HIGH if button connects to VCC
+    */
+    Button(byte attachTo, bool active = LOW) : buttonPin(attachTo), active(active) {}
+
+    /**
+       \brief set the pin to the proper state
+
+       Call this function in your setup().
+       The pinMode will be set according to your constructor.
+    */
+    void begin()
+    {
+        if (active == LOW)
+            pinMode(buttonPin, INPUT);
+        else
+            pinMode(buttonPin, INPUT_PULLUP);
+    }
+
+    /**
+        \brief indicate if button was pressed since last call
+
+        @return HIGH if button was pressed since last call - debounce
+    */
+    bool wasPressed()
+    {
+        bool buttonState = LOW; // the current reading from the input pin
+        byte reading = LOW;     // "translated" state of button LOW = released, HIGH = pressed, despite the electrical state of the input pint
+        if (digitalRead(buttonPin) == active)
+            reading = HIGH;                                         // if we are using INPUT_PULLUP we are checking invers to LOW Pin
+        if (((millis() & 0xFF) - lastDebounceTime) > debounceDelay) // If the switch changed, AFTER any pressing or noise
+        {
+            if (reading != lastButtonState && lastButtonState == LOW) // If there was a change and and last state was LOW (= released)
+            {
+                buttonState = HIGH;
+            }
+            lastDebounceTime = millis() & 0xFF;
+            lastButtonState = reading;
+        }
+        return buttonState;
+    }
+};
+
+Button buttonBack{BUTTON_BACK_PIN};
+Button buttonOK{BUTTON_OK_PIN};
+Button buttonNext{BUTTON_NEXT_PIN};
+
+void beep(long freq)
+{
+    tone(BUZZER_PIN, freq, 125);
+}
+
 class OLEDModule
 {
 public:
@@ -37,6 +110,7 @@ public:
         }
         displayLogo();
         delay(2000);
+        displayMenu();
     }
 
     void displayLogo()
@@ -196,6 +270,49 @@ public:
         oled.display();
     }
 
+    void displayMenu()
+    {
+        oled.clearDisplay();
+        oled.setTextSize(1);
+        oled.setTextColor(WHITE);
+        oled.setCursor(0, 2);
+
+        for (int i = 0; i < menuOptions.size(); i++)
+        {
+            if (i == selectedOption)
+            {
+                oled.print("> "); // Display ">" for the selected option
+            }
+            else
+            {
+                oled.print("  "); // Display a space for other options
+            }
+            oled.println(menuOptions[i]);
+        }
+
+        oled.display();
+    }
+
+    void navigateMenu()
+    {
+        if (buttonBack.wasPressed())
+        {
+            selectedOption = (selectedOption - 1 + menuOptions.size()) % menuOptions.size();
+            displayMenu();
+            beep(3000);
+        }
+        if (buttonOK.wasPressed())
+        {
+            beep(2000);
+        }
+        if (buttonNext.wasPressed())
+        {
+            selectedOption = (selectedOption + 1) % menuOptions.size();
+            displayMenu();
+            beep(3000);
+        }
+    }
+
     void monitorResources(float power, float current, float resistance, float voltage)
     {
         oled.clearDisplay();
@@ -212,73 +329,15 @@ public:
         oled.println(voltage);
         oled.display();
     }
+
+private:
+    int selectedOption;
+    std::vector<String> menuOptions = {
+        "Option 1",
+        "Option 2",
+        "Option 3",
+    };
 };
-
-class Button
-{                                             // a simple class for buttons based on the "Debounce" example
-    const byte buttonPin;                     // the GPIO / pin for the button
-    static constexpr byte debounceDelay = 30; // the debounce time; increase if the output flickers. Static because we only need one value for all buttons
-    const bool active;                        // is the pin active HIGH or active LOW (will also activate the pullups!)
-    bool lastButtonState = HIGH;              // the previous reading from the input pin
-    byte lastDebounceTime = 0;                // the last time the output pin was toggled - we check only ONE byte, so I didn't mess around with unsigned long
-
-public:
-    /**
-       \brief constructor for a button
-
-       The constructor takes the GPIO as parameter.
-       If you omit the second parameter, the library will activate the internal pullup resistor
-       and the button should connect to GND.
-       If you set the second parameter to HIGH, the button is active HIGH.
-       The button should connect to VCC.
-       The internal pullups will not be used but you will need an external pulldown resistor.
-
-       \param attachTo the GPIO for the button
-       \param active LOW (default) - if button connects to GND, HIGH if button connects to VCC
-    */
-    Button(byte attachTo, bool active = LOW) : buttonPin(attachTo), active(active) {}
-
-    /**
-       \brief set the pin to the proper state
-
-       Call this function in your setup().
-       The pinMode will be set according to your constructor.
-    */
-    void begin()
-    {
-        if (active == LOW)
-            pinMode(buttonPin, INPUT);
-        else
-            pinMode(buttonPin, INPUT_PULLUP);
-    }
-
-    /**
-        \brief indicate if button was pressed since last call
-
-        @return HIGH if button was pressed since last call - debounce
-    */
-    bool wasPressed()
-    {
-        bool buttonState = LOW; // the current reading from the input pin
-        byte reading = LOW;     // "translated" state of button LOW = released, HIGH = pressed, despite the electrical state of the input pint
-        if (digitalRead(buttonPin) == active)
-            reading = HIGH;                                         // if we are using INPUT_PULLUP we are checking invers to LOW Pin
-        if (((millis() & 0xFF) - lastDebounceTime) > debounceDelay) // If the switch changed, AFTER any pressing or noise
-        {
-            if (reading != lastButtonState && lastButtonState == LOW) // If there was a change and and last state was LOW (= released)
-            {
-                buttonState = HIGH;
-            }
-            lastDebounceTime = millis() & 0xFF;
-            lastButtonState = reading;
-        }
-        return buttonState;
-    }
-};
-
-Button buttonBack{BUTTON_BACK_PIN};
-Button buttonOK{BUTTON_OK_PIN};
-Button buttonNext{BUTTON_NEXT_PIN};
 
 OLEDModule oledmod;
 
@@ -307,28 +366,7 @@ void loop()
         previousDimMillis = currentMillis;
     }
 
-    receiveInput();
-}
-
-void receiveInput()
-{
-    if (buttonBack.wasPressed())
-    {
-        beep(3000);
-    }
-    if (buttonOK.wasPressed())
-    {
-        beep(2000);
-    }
-    if (buttonNext.wasPressed())
-    {
-        beep(3000);
-    }
-}
-
-void beep(long freq)
-{
-    tone(BUZZER_PIN, freq, 125);
+    oledmod.navigateMenu();
 }
 
 void dim()
@@ -344,6 +382,6 @@ float measure(float input)
     float current = voltage / resistance;
     float power = voltage * current;
 
-    oledmod.monitorResources(power, current, resistance, voltage);
+    // oledmod.monitorResources(power, current, resistance, voltage);
     return input;
 }
